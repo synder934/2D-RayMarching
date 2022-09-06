5f9a489c13b3c4d6ed92db6722ff67dc93e6055b
synder934 <tobyred8@hotmail.co.uk>
2022-09-06 16:53:11 +0100

    better now

--- main.py
+++ main.py
@@ -1,30 +1,62 @@
+from random import randint
 import pygame as pg
 
+DIM = [1000, 700]
+FPS = 60
+vec = pg.Vector2
 
-class Game():
+class Player():
     def __init__(self) -> None:
-        pg.init()
+        self.pos = vec(pg.mouse.get_pos())
+        self.rays = [Ray(i) for i in range(0, 360)]
+        pass
 
-        self.disp = pg.display.set_mode((600,600))
-
-        self.mainloop()
-
-
-    def mainloop(self):
-        while True:
-            self.disp.fill((100,100,100))
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.quit()
-
-            pg.display.update()
+    def update(self):
+        self.pos = vec(pg.mouse.get_pos())
+        pass
 
 
-    def quit(self):
-        pg.quit()
-        quit()
+class Ray():
+    def __init__(self, angle) -> None:
+        self.angle = angle
 
+        pass
+
+    def update(self):
+        
+        pass
+
+class Boundary():
+    def __init__(self, start:tuple, end:tuple) -> None:
+        self.start = vec(start)
+        self.end = vec(end)
+        pass
+
+
+def genBoundaries(n):
+    return [Boundary((randint(0, DIM[0]), randint(0, DIM[1])), (randint(0, DIM[0]), randint(0, DIM[1]))) for i in range(n)]
 
 
 if __name__ == '__main__':
-    Game()
+    pg.init()
+    disp = pg.display.set_mode(DIM)
+    clock = pg.time.Clock()
+    bounds = genBoundaries(1)
+
+    running = 1
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = 0
+            if event.type == pg.MOUSEBUTTONUP:
+                bounds = genBoundaries(5)
+        disp.fill((0,0,0))
+
+        for b in bounds:
+            pg.draw.line(disp, (255,255,255), b.start, b.end)
+
+        
+
+
+        pg.display.update()
+        clock.tick(FPS)
