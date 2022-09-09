7b28ba3878523f56f5c4cbc8502360f64060ce0d
synder934 <tobyred8@hotmail.co.uk>
2022-09-09 13:28:34 +0100

    now with toggelable RGB

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from copy import deepcopy
 from math import cos, radians, sin
+import colorsys
 from random import randint
 import pygame as pg
 
-DIM = [1000, 700]
+DIM = [1920, 1080]
+
 vec = pg.Vector2
 
 FPS = 60
 CIR_RES = 2
 TURN_SPEED = 5
-FOV = 70
+FOV = 360
+RGB = True
+
 
 class Player():
     def __init__(self) -> None:
@@ -18,10 +21,12 @@ class Player():
         self.rays = [Ray(self, radians(i/CIR_RES)) for i in range(0, int(FOV*CIR_RES))]
         pass
 
+
     def update(self):
         self.pos = vec(pg.mouse.get_pos())
         try:
             list(map(lambda x: x.update(), self.rays))
+            pg.draw.polygon(disp, (100,100,100), [x.epos for x in self.rays].append(self.pos))
         except: pass
 
 class Ray():
@@ -29,32 +34,27 @@ class Ray():
         self.angle = angle
         self.player = player
         self.epos = self.dir()*10
-        
-        pass
+
 
     def dir(self):
         return vec(cos(self.angle+self.player.rot), sin(self.angle+self.player.rot))
         
+
     def calcIntersect(self):
         results = []
-
         x3 = self.player.pos.x
         y3 = self.player.pos.y
         x4 = self.player.pos.x + self.dir().x
         y4 = self.player.pos.y + self.dir().y
-
         for w in bounds:
             x1 = w.start.x
             y1 = w.start.y
             x2 = w.end.x
             y2 = w.end.y
-
             den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
             if den == 0: continue
-
             t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
             u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
-
             if t > 0 and t < 1 and u > 0:
                 results.append(
                     vec(
@@ -62,11 +62,9 @@ class Ray():
                         y1 + t * (y2 - y1)
                     )
                 )
-            else: pass
-
         results.sort(key = lambda x: ((x.x-self.player.pos.x)**2+(x.y-self.player.pos.y)**2)**0.5)
-
         return results[0]
+
 
     def update(self):
         self.epos = self.calcIntersect()
@@ -80,6 +78,7 @@ class Boundary():
         pass
 
 
+
 def genBoundaries(n, walls = True):
     ret= [Boundary((randint(0, DIM[0]), randint(0, DIM[1])), (randint(0, DIM[0]), randint(0, DIM[1]))) for i in range(n)]
     if walls:
@@ -88,6 +87,7 @@ def genBoundaries(n, walls = True):
         ret.append(Boundary(DIM, (-1, DIM[1])))
         ret.append(Boundary(DIM, (DIM[0], -1)))
     return ret
+
 
 if __name__ == '__main__':
     pg.init()
@@ -113,17 +113,21 @@ if __name__ == '__main__':
                     player.rot-= radians(TURN_SPEED)
                 elif e.text == 'd':
                     player.rot+= radians(TURN_SPEED)
+                elif e.text == 'r':
+                    RGB = not RGB
 
 
         disp.fill((0,0,0))
 
         player.update()
-
-        for r in player.rays:
-            pg.draw.line(disp, (100,100,100), player.pos, r.epos)
+        
+        for i, r in enumerate(player.rays):
+            c = colorsys.hsv_to_rgb(i/len(player.rays), 1, 1) if RGB else (100/255,100/255,100/255)
+            c = (c[0]*255, c[1]*255, c[2]*255)
+            pg.draw.line(disp, c, player.pos, r.epos)
 
         for b in bounds:
             pg.draw.line(disp, (255,255,255), b.start, b.end)
 
         pg.display.update()
-        clock.tick(FPS)
+        #clock.tick(FPS)
